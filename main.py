512a949bb54a86c319746407cbe1154055d8aa42
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-07 20:36:42 +0430

    Importing relevant packages

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
     Packages: numpy-stl, mpl_toolkits, matplotlib
     @author: Milad Sadeghi DM - EverLookNeverSee@GiHub
 """
+
+
+from stl import mesh
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
