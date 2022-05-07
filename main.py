48f7f4dfb9013de0ae8dad4b6d19ca0eb62875e0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-07 20:40:08 +0430

    Extracting mass properties and plotting it

--- main.py
+++ main.py
@@ -22,3 +22,9 @@ axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 # Performing autoscaling
 scale = your_mesh.points.flatten()
 axes.auto_scale_xyz(scale, scale, scale)
+
+# Extracting mass properties and plotting it
+volume, cog, inertia = your_mesh.get_mass_properties()
+print(f"Volume: {volume},\nPosition of the center of gravity (COG): {cog},\nInertia: {inertia}")
+# Show the plot to the screen
+pyplot.show()
