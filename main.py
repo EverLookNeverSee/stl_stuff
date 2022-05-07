2934c72c107873865108dc930e55ff9f78ee0fa4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-07 20:37:57 +0430

    Specifying stl file path and loading it

--- main.py
+++ main.py
@@ -8,3 +8,8 @@
 from stl import mesh
 from matplotlib import pyplot
 from mpl_toolkits import mplot3d
+
+# stl file path
+path = "/home/elns/Projects/stl_stuff/test_stl_files/Eiffel_tower_sample.STL"
+# Loading stl file
+your_mesh = mesh.Mesh.from_file(path, calculate_normals=False, speedups=True)
