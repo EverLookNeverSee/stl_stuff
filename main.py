806926eb8e0372dcc26534b62c815399e0b4bf4a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-07 20:38:43 +0430

    Setting up the figure

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ from mpl_toolkits import mplot3d
 path = "/home/elns/Projects/stl_stuff/test_stl_files/Eiffel_tower_sample.STL"
 # Loading stl file
 your_mesh = mesh.Mesh.from_file(path, calculate_normals=False, speedups=True)
+
+# Setting up the figure
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
